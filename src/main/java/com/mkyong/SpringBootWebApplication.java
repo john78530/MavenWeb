f74d65d978ffc9cf978c2c32b5c8fc1a3b27c7db
john <john78530@yahoo.com.tw>
2017-07-25 15:44:35 +0800

    Add Spark Framework

--- src/main/java/com/mkyong/SpringBootWebApplication.java
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import spark.TemplateEngine;
-//import spark.template.velocity.VelocityTemplateEngine;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,11 +20,11 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) throws Exception {
-//		get("/hello", (request, response) -> {
-//			Map<String, Object> model = new HashMap<>();
-//			model.put("Title", "三聯式發票");
-//			return new ModelAndView(model, "application.properties");
-//		}, new VelocityTemplateEngine());
+		get("/hello", (request, response) -> {
+			Map<String, Object> model = new HashMap<>();
+			model.put("Title", "三聯式發票");
+			return new ModelAndView(model, "application.properties");
+		}, new VelocityTemplateEngine());
 		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 
